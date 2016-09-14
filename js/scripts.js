9da314cc318c98ce5bfa182fb55a08f68dbc1258
Nicholas Green <nicholasgreen94@gmail.com>
2016-09-14 10:20:25 -0700

    Updated look of web and design sections

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(function() {
   
 // initial variables
 var webpage = $("html, body");
+var $close_icon = $(".close-icon");
+var $menu_icon = $(".menu-icon");
 var $project_description = $(".project-description");
 var $btn_wrap = $(".btn-wrap");
 var $btn_reveal = $(".btn-reveal");
@@ -9,60 +11,82 @@ var $btn_minimize = $(".btn-minimize");
 
 // initially hiding work
 $project_description.hide();
-$btn_wrap.hide();
+
+$close_icon.hide();
+
+$menu_icon.on("click", function() {
+  $(this).hide();
+  $close_icon.show();
+});
+
+$close_icon.on("click", function() {
+  $(this).hide();
+  $menu_icon.show();
+});
+
 
 // When user clicks on the reveal button, slide down the hidden p and .btn-wrap
 // remove class of .project-initial and add class of .project-click
 // remove current grid sizes for bigger grid sizes
 $btn_reveal.on("click", function() {
   $(this).hide(); // hide button
-  $(this).parent("section")
-            .slideDown(500)
-            .removeClass("large-5 large-offset-1")
-            .addClass("large-offset-1 large-10"); // show .project-p
+  $(this).parent("div")
+            .parent("article")
+            .parent("section")
+            .removeClass("large-7 large-5") // Change size from original to fill whole row
             
-  $(this).next()
+  $(this).parent("div")
+            .parent("article")
+            .find($btn_wrap)
             .show(); // Show .btn-wrap
             
-  $(this).parent("section")
-            .find("article")
+  $(this).parent("div")
+            .parent("article")
             .removeClass("project-initial")
             .addClass("project-click"); // remove .project-inital, add .project-click
             
-  $(this).parent("section")
+  $(this).parent("div")
+            .parent("article")
             .find(".project-description")
-            .slideDown(); // Show .project-description
+            .fadeIn(); // Show .project-description
 
-  $(this).parent("section")
+  $(this).parent("div")
+            .parent("article")
+            .parent("section")
             .next("section")
-            .removeClass("large-5 large-offset-1 large-offset-0")
-            .addClass("large-offset-1 large-10"); // Find next section and give it the following classes
+            .removeClass("large-7 large-5")
+            .addClass("large-12"); // Find next section and give it the following classes
   
-  $(this).parent("section")
+  $(this).parent("div")
+            .parent("article")
+            .parent("section")
             .prev("section")
-            .removeClass("large-5 large-offset-1 large-offset-0 end")
-            .addClass("large-offset-1 large-10"); // give the next section the following classes
+            .removeClass("large-7 large-5 end")
+            .addClass("large-12"); // give the next section the following classes
 });
   
 // When user clicks on the minimize button, slide up .project-p and .btn-wrap
 // add class of .project-initial and remove class of .project-click
 // fade in the reveal button
 $btn_minimize.on("click", function() {
-  $(this).parent(".btn-wrap")
-  .hide(); // hide .btn_wrap
+  $(this).parent($btn_wrap)
+            .fadeOut(); // hide .btn_wrap
   
-  $(this).parent(".btn-wrap")
-  .parent("section")
-  .find(".project-description")
-  .slideUp(); // hide .project-description
+  $(this).parent($btn_wrap)
+            .parent("div")
+            .fadeOut(); // hide .project-description
   
-  $(this).parent(".btn-wrap")
-  .parent("section")
-  .find("article")
-  .removeClass("project-click")
-  .addClass("project-initial"); // remove .project-click, add .project-initial
+  $(this).parent($btn_wrap)
+            .parent("div")
+            .parent("article")
+            .removeClass("project-click")
+            .addClass("project-initial"); // remove .project-click, add .project-initial
 
-  $btn_reveal.show(); // show reveal button
+  $(this).parent($btn_wrap)
+            .parent("div") 
+            .next()
+            .find("button")
+            .show(); // show reveal button
 });
   
   
